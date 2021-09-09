ba8ffcf27839a763cc17fdfb54350dd53b257419
노랑끄투 <72529713+yellowoof24@users.noreply.github.com>
2021-09-09 21:51:11 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+import re
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yellowoof24ishacker'
+socketio = SocketIO(app)
+
+@app.route("/")
+def chat():
+    return render_template("chat.html")
+
+@socketio.on('chat')
+def join(data, methods=['GET', 'POST']):
+    data["msg"] = re.sub(r'(<([^>]+)>)', "", data["msg"], flags=re.MULTILINE)
+    data["name"] = re.sub(r'(<([^>]+)>)', "", data["name"], flags=re.MULTILINE)
+    print(data["msg"])
+    socketio.emit('chat', data)
+
+@socketio.on('join')
+def join(data, methods=['GET', 'POST']):
+    data["name"] = re.sub(r'(<([^>]+)>)', "", data["name"], flags=re.MULTILINE)
+    socketio.emit('join', data)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
