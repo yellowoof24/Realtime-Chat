befaea04d5a56812f739b2aa5afac39f6850dcb1
노랑끄투 <72529713+yellowoof24@users.noreply.github.com>
2021-09-09 22:19:32 +0900

    수정

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def chat():
     return render_template("chat.html")
 
 @socketio.on('chat')
-def join(data, methods=['GET', 'POST']):
+def chat(data, methods=['GET', 'POST']):
     data["msg"] = re.sub(r'(<([^>]+)>)', "", data["msg"], flags=re.MULTILINE)
     data["name"] = re.sub(r'(<([^>]+)>)', "", data["name"], flags=re.MULTILINE)
     print(data["msg"])
